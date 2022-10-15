dd71d646c761db0fa5cd14e9644ac65532662b2e
Ryder Casazza <84256232+campad@users.noreply.github.com>
2022-10-15 07:54:26 -0700

    DeployIntake

--- src/main/java/frc/robot/commands/Intake/DeployIntakeCommand.java
+++ src/main/java/frc/robot/commands/Intake/DeployIntakeCommand.java
@@ -17,6 +17,7 @@ public class DeployIntakeCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    m_intakeSubsystem.deployIntake(true);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
